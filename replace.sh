643ae4dfee0f3a87bbb34f80188c24d2175eae33
xuancaishaun <xuancaishaun@gmail.com>
2013-04-30 22:12:03 +0800

    1

--- replace.sh
+++ replace.sh
@@ -3,5 +3,5 @@ cp ./external_gateway.py ../fyp_nep2p/lib
 cp ./scheduler.py ../fyp_nep2p/lib
 cp ./run.py ../fyp_nep2p
 cp ./logger.py ../fyp_nep2p/lib/util
-cp ./config.json ../fyp_nep2p/lib/util
+cp ./config.json ../fyp_nep2p/
 cp ./app_worker.py ../fyp_nep2p/lib/util
