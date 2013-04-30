619d140bbb3daefc4755858a6dd2738ad7fff2b2
xuancaishaun <xuancaishaun@gmail.com>
2013-04-30 22:46:50 +0800

    updated app_worker.py

--- replace.sh
+++ replace.sh
@@ -4,4 +4,4 @@ cp ./scheduler.py ../fyp_nep2p/lib
 cp ./run.py ../fyp_nep2p
 cp ./logger.py ../fyp_nep2p/lib/util
 cp ./config.json ../fyp_nep2p/
-cp ./app_worker.py ../fyp_nep2p/lib/util
+cp ./app_worker.py ../fyp_nep2p/lib/
