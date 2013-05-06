7d059af34fc515d537a4ce474715e24a0ec14210
xuancaishaun <xuancaishaun@gmail.com>
2013-05-06 04:31:52 +0000

    modified logger.py

---
+++ nep2p2_a16/logger.py
@@ -0,0 +1,78 @@
+import os, json, time
+
+try:
+	client_config = json.loads(open('config.json', 'r').read())
+except:
+	client_config = {}
+
+LOG_FILE_BASE = client_config['log_file_base'].encode() if 'log_file_base' in client_config else 'log/default/'
+
+class Logger(object):
+	def __init__(self, filepath, flags="w+"):
+		directory = os.path.abspath(os.path.dirname(filepath))
+		if not os.path.exists(directory):
+			os.makedirs(directory)
+		self._fd = open(filepath, flags)
+
+	def log(self, msg):
+		if not self._fd:
+			return
+		self._fd.write(msg)
+
+	def logline(self, msg):
+		self.log(msg)
+		self.log('\n')
+
+	def close(self):
+		if self._fd:
+			self._fd.close()
+			self._fd = None
+	
+class schLogger(Logger):# Scheduler.py
+	def __init__(self):
+		self.filepath = LOG_FILE_BASE + 'snd.log'
+		Logger.__init__(self, self.filepath)
+
+	def start(self):
+		self.logline('# Start of logging: ' + time.ctime())
+		self.count = dict() # {(ip, asid): (timestamp, count)}
+
+	def stop(self):
+		for peer in self.count.keys():
+                        curCount = self.count[peer]
+                        self.logline('{0}; {1}; {2};'.format(peer, curCount[0], curCount[1]))
+                self.logline('# End of logging: ' + time.ctime())
+                self.close()
+	
+	def logSentPkt(self, remote, curTime, precision, pkts_num):
+		curTime = float('%{0}f'.format(precision)%float(curTime))
+		# convert the time to specified precision
+		curCount = self.count.get(remote)
+		if curCount == None:
+			self.count[remote] = (curTime, pkts_num)
+		elif curTime == curCount[0]:
+			self.count[remote] = (curTime, curCount[1] + pkts_num)
+		else:
+			self.logline('{0}; {1}; {2};'.format(remote, curCount[0], curCount[1]))
+			self.count[remote] = (curTime, pkts_num)
+
+
+if __name__ == "__main__":
+	l = schLogger()
+	l.start()
+	l.logSentPkt('("1.1.1.1", 3000)', '1111.111', '0.1', 10)
+	l.logSentPkt('("1.1.1.1", 3000)', '1111.111', '0.1', 20)
+	l.logSentPkt('("1.1.1.1", 3000)', '1112.111', '0.1', 30)
+	l.stop()
+	'''
+	l = Logger("log/test.log")
+	l.log("abc")
+	l.logline("def")
+	l.logline("ghi")
+	l.close()
+
+	import time
+	l = Logger("log/timetest.log","a+")
+	l.logline("%f ghi"%(time.time()))
+	l.close()
+	'''
