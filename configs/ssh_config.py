e5820ee7bf795d7dd88499752dac82ecd1ef3b8e
xuancaishaun <xuancaishaun@gmail.com>
2013-05-09 08:36:28 +0000

    config.json and py

---
+++ configs/ssh_config.py
@@ -0,0 +1,12 @@
+import json
+
+nodes = json.loads(open('ssh_config.json', 'r').read())
+out = open('config', 'w+')
+
+for key in nodes.keys():
+	out.write('Host %s\n'%key.encode())
+	out.write('\tHostName %s\n'%nodes.get(key).encode())
+	out.write('\tUser cuhk_inc_01\n')
+	out.write('\tIdentifyFile ~/.ssh/planetlab2_rsa\n\n')
+
+
