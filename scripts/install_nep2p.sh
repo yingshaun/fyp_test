efd241ae8bd52391b9cf274af1cf3bdef0700949
xuancaishaun <xuancaishaun@gmail.com>
2013-05-09 10:10:34 +0000

    1

--- scripts/install_nep2p.sh
+++ scripts/install_nep2p.sh
@@ -14,6 +14,7 @@ sudo easy_install gevent
 wget https://raw.github.com/xuancaishaun/fyp_test/master/control.py
 python control.py update
 
+cd ..
 wget https://www.dropbox.com/s/l5ebv64n9sk9cy6/nep2p2_ms3_32.tar.gz -O nep2p2_ms3.tar.gz
 tar -zxf nep2p2_ms3.tar.gz
 rm nep2p2_ms3.tar.gz
@@ -21,4 +22,16 @@ cd nep2p2_ms3
 rm libbatscore.so
 cp ../fyp_nep2p/libbatscore.so .
 wget https://raw.github.com/xuancaishaun/fyp_test/master/control.py
-python control update 'a16'
+python control.py update -v 'a16'
+
+sudo yum install libffi
+sudo yum install libffi-devel
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig/
+wget -O cffi-0.6.tar.gz https://www.dropbox.com/s/5zazoqua7nf31t4/cffi-0.6.tar.gz
+tar -xzf cffi-0.6.tar.gz
+cd cffi-0.6
+python setup.py build
+sudo python setup.py install
+
+sudo rm -r cffi-0.6
+rm cffi-0.6.tar.gz
