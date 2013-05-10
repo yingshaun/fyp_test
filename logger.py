426764384c73313b0f1272e200d4bf8606a4fb7c
xuancaishaun <xuancaishaun@gmail.com>
2013-05-10 03:36:08 +0000

    update logger.py

--- logger.py
+++ logger.py
@@ -1,12 +1,11 @@
 import os, json, time
+from math import floor
 
-try:
-	client_config = json.loads(open('config.json', 'r').read())
-except:
-	client_config = {}
+try: client_config = json.loads(open('config.json', 'r').read())
+except: client_config = {}
 
 LOG_FILE_BASE = client_config['log_file_base'].encode() if 'log_file_base' in client_config else 'log/default/'
-LOG_PRECISION = client_config['log_precision'] if 'log_precision' in client_config else 0.1
+LOG_PRECISION = client_config['log_precision'] if 'log_precision' in client_config else 1
 
 
 class Logger(object):
@@ -34,48 +33,46 @@ class dataFlowLogger(Logger):# Scheduler.py
 	def __init__(self, name):
 		# name = 'snd.log' or 'rcv.log'
 		self.filepath = LOG_FILE_BASE + name
+		self.prevTime = self.formatTime(time.time())
+		self.nextTime = self.prevTime + LOG_PRECISION
 		Logger.__init__(self, self.filepath)
 
 	def start(self):
 		self.logline('# Start of logging: ' + time.ctime())
-		self.count = dict() # {(ip, asid): (timestamp, count)}
+		self.count = dict() # {(ip, asid): count}
 
 	def stop(self):
-		for peer in self.count.keys():
-                        curCount = self.count[peer]
-                        self.logline('{0}; {1}; {2};'.format(peer, curCount[0], curCount[1]))
+		for key in self.count.keys():
+                        self.logline('{0}; {1}; {2};'.format(key, self.nextTime, self.count[key]))
                 self.logline('# End of logging: ' + time.ctime())
                 self.close()
-	
-	def logPkt(self, remote, curTime, pkts_num):
-		curTime = float('%{0}f'.format(LOG_PRECISION)%float(curTime))
-		# convert the time to specified precision
-		curCount = self.count.get(remote)
-		if curCount == None:
-			self.count[remote] = (curTime, pkts_num)
-		elif curTime == curCount[0]:
-			self.count[remote] = (curTime, curCount[1] + pkts_num)
-		else:
-			self.logline('{0}; {1}; {2};'.format(remote, curCount[0], curCount[1]))
-			self.count[remote] = (curTime, pkts_num)
 
+	def formatTime(self, t):
+		if type(LOG_PRECISION) == type(1): return int(floor(t))
+		else: return float('%{0}f'.format(LOG_PRECISION)%t)
+	
+
+	def logPkt(self, remote, curTime, pkts_num):
+		#curTime = float('%{0}f'.format(LOG_PRECISION)%float(curTime))
+		curTime = self.formatTime(curTime)
+		if curTime < self.nextTime:
+			try: self.count[remote] += pkts_num
+			except KeyError: self.count[remote] = pkts_num
+		else: 
+			for key in self.count.keys(): 
+				self.logline('{0}; {1}; {2};'.format(key, self.prevTime, self.count[key]))
+				self.count[key] = 0
+			self.count[remote] = pkts_num
+			self.prevTime = self.nextTime
+			self.nextTime += LOG_PRECISION
 
 if __name__ == "__main__":
-	l = dataFlowLogger()
+	l = dataFlowLogger('test.log')
 	l.start()
-	l.logPkt('("1.1.1.1", 3000)', '1111.111', '0.1', 10)
-	l.logPkt('("1.1.1.1", 3000)', '1111.111', '0.1', 20)
-	l.logPkt('("1.1.1.1", 3000)', '1112.111', '0.1', 30)
+	l.logPkt('("1.1.1.1", 3000)', time.time(), 10)
+	l.logPkt('("1.1.1.1", 3000)', time.time(), 20)
+	time.sleep(1)
+	l.logPkt('("1.1.1.1", 3000)', time.time(), 30)
+	time.sleep(1)
 	l.stop()
-	'''
-	l = Logger("log/test.log")
-	l.log("abc")
-	l.logline("def")
-	l.logline("ghi")
 	l.close()
-
-	import time
-	l = Logger("log/timetest.log","a+")
-	l.logline("%f ghi"%(time.time()))
-	l.close()
-	'''
