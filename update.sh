32e8934f0924c6750a2afcf49aa4c8729ac500a7
xuancaishaun <xuancaishaun@gmail.com>
2013-05-11 06:24:17 +0000

    added update.sh

---
+++ update.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ "$1" == "-h" ]; then
+	echo "Usage: `basename $0` --option"
+	exit 0
+fi
+
+if [ "$1" == "a13" ]
+then
+	wget -O lib/external_gateway.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_a13/external_gateway.py
+	wget -O lib/scheduler.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_a13/scheduler.py
+	wget -O lib/app_worker.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_a13/app_worker.py
+
+	wget -O lib/util/logger.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_a13/logger.py
+
+	wget -O statCollect.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_a13/statCollect.py
+	wget -O run.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_a13/run.py
+	wget -O cli.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_a13/cli.py
+	wget -O ddM16m8r92TO.txt https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_a13/ddM16m8r92TO.txt
+
+elif [ "$1" == "a16" ]
+then
+	wget -O lib/external_gateway.py https://raw.github.com/xuancaishaun/fyp_test/master/external_gateway.py
+	wget -O lib/scheduler.py https://raw.github.com/xuancaishaun/fyp_test/master/scheduler.py
+	wget -O lib/app_worker.py https://raw.github.com/xuancaishaun/fyp_test/master/app_worker.py
+
+	wget -O lib/util/logger.py https://raw.github.com/xuancaishaun/fyp_test/master/logger.py
+	wget -O lib/util/ip.py https://raw.github.com/xuancaishaun/fyp_test/master/ip.py
+
+	wget -O statCollect.py https://raw.github.com/xuancaishaun/fyp_test/master/statCollect.py
+	wget -O run.py https://raw.github.com/xuancaishaun/fyp_test/master/run.py
+	wget -O cli.py https://raw.github.com/xuancaishaun/fyp_test/master/cli.py
+	wget -O ddM16m8r92TO.txt https://raw.github.com/xuancaishaun/fyp_test/master/ddM16m8r92TO.txt
+fi
