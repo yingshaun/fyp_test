3486daa939fb34ae9ce6fe02f900ebb89ace0bd7
xuancaishaun <xuancaishaun@gmail.com>
2013-05-11 15:26:31 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -18,7 +18,7 @@ then
 	wget -O cli.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_a13/cli.py
 	wget -O ddM16m8r92TO.txt https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_a13/ddM16m8r92TO.txt
 
-elif [ "$1" == "a16" ]
+elif [ "$1" == "a16" || "$1" == 'a16_rudp' ]
 then
 	wget -O lib/external_gateway.py https://raw.github.com/xuancaishaun/fyp_test/master/external_gateway.py
 	wget -O lib/scheduler.py https://raw.github.com/xuancaishaun/fyp_test/master/scheduler.py
@@ -33,4 +33,13 @@ then
 	wget -O run.py https://raw.github.com/xuancaishaun/fyp_test/master/run.py
 	wget -O cli.py https://raw.github.com/xuancaishaun/fyp_test/master/cli.py
 	wget -O ddM16m8r92TO.txt https://raw.github.com/xuancaishaun/fyp_test/master/ddM16m8r92TO.txt
+
+	if [ "$1" == "a16_rudp" ]
+	then
+		wget -O lib/appsocket.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/appsocket.py
+		wget -O lib/gateway.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/gateway.py
+		wget -O lib/rudp.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/rudp.py
+		wget -O lib/run.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/run.py
+	fi
+
 fi
