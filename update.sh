79cabb2c4911a66af32d90b298fcf6558405a827
xuancaishaun <xuancaishaun@gmail.com>
2013-05-11 15:45:34 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -42,4 +42,21 @@ then
 		wget -O run.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/run.py
 	fi
 
+elif [ "$1" == "a13_to_a16" ]
+then
+	sudo yum install libffi
+	sudo yum install libffi-devel
+	export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig/
+	wget -O cffi-0.6.tar.gz https://www.dropbox.com/s/5zazoqua7nf31t4/cffi-0.6.tar.gz
+	tar -zxf cffi-0.6.tar.gz
+	cd cffi-0.6
+	python setup.py build
+	sudo python setup.py install
+	cd ..
+	wget -O nep2p2_a16.zip https://www.dropbox.com/s/sanoagaumn9iyd8/nep2p2_a16.zip
+	sudo yum install unzip
+	unzip nep2p2_a16.zip
+	sudo rm -r __MACOSX
+	sudo rm -r bats_clean
+	cp fyp_nep2p/libbatscore.so nep2p2_ms3/
 fi
