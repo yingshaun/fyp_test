e02d6c300ed286b87bf09431620ee46ccb0d1fad
xuancaishaun <xuancaishaun@gmail.com>
2013-05-11 15:27:11 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -39,7 +39,7 @@ then
 		wget -O lib/appsocket.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/appsocket.py
 		wget -O lib/gateway.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/gateway.py
 		wget -O lib/rudp.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/rudp.py
-		wget -O lib/run.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/run.py
+		wget -O run.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/run.py
 	fi
 
 fi
