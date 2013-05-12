6da73588e5c9510e8347d99e9406564bab6b8bb2
xuancaishaun <xuancaishaun@gmail.com>
2013-05-12 03:36:40 +0000

    1

--- update.sh
+++ update.sh
@@ -41,6 +41,7 @@ then
 		wget -O lib/appsocket.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/appsocket.py
 		wget -O lib/gateway.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/gateway.py
 		wget -O lib/rudp.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/rudp.py
+		wget -O run.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/run.py
 	fi
 elif [ "$1" == "a16_rudp_to_a16" ]
 then
