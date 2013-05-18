fd2933b44f84001aeb1378efbc3d4f78ceb3249d
xuancaishaun <xuancaishaun@gmail.com>
2013-05-18 12:25:49 +0800

    Update install_openssh.sh

--- scripts/install_openssh.sh
+++ scripts/install_openssh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 /sbin/service sshd status 		#check if ssh-server is installed or not
-sudo yum install openssh-server 	#install the package
+sudo yum -y install openssh-server 	#install the package
 sudo /sbin/service sshd restart		#start the service
