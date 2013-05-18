7c4e0556a2de0adb81cf321ee19b95dd99140bd5
xuancaishaun <xuancaishaun@gmail.com>
2013-05-18 12:26:11 +0800

    Update install_cffi.sh

--- scripts/install_cffi.sh
+++ scripts/install_cffi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo yum install libffi
-sudo yum install libffi-devel
+sudo yum -y install libffi
+sudo yum -y install libffi-devel
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig/
 wget -O cffi-0.6.tar.gz https://www.dropbox.com/s/5zazoqua7nf31t4/cffi-0.6.tar.gz
 tar -xzf cffi-0.6.tar.gz
