3db0228baead6a86b2050cf2929f39d16e652adc
xuancaishaun <xuancaishaun@gmail.com>
2013-05-18 03:32:19 +0000

    remove files

--- scripts/update_a16_rudp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget -O run.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/run.py
-wget -O lib/appsocket.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/appsocket.py
-wget -O lib/rudp.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/rudp.py
-wget -O lib/gateway.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/gateway.py
