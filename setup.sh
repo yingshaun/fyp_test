eef9f465902afa036be3c34dfe841b4f140066de
xuancaishaun <xuancaishaun@gmail.com>
2013-06-09 11:25:20 +0000

    moved from kalasoo/p2p_test

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir genfiles
+mkdir getfiles
+mkdir torrents
+
+wget https://www.dropbox.com/s/pa7syp5krrymhsu/Key.zip
+unzip Key.zip
+rm Key.zip
+
+wget https://raw.github.com/xuancaishaun/fyp_test/master/configs/nodes_addr.json
+
---
+++ update_control.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -O genfiles/control.py https://raw.github.com/xuancaishaun/fyp_test/master/control.py
+wget -O genfiles/btControl.py https://raw.github.com/xuancaishaun/fyp_test/master/btControl.py
+wget -O genfiles/logger.py https://raw.github.com/xuancaishaun/fyp_test/master/logger.py
+wget -O genfiles/statCollect.py https://raw.github.com/xuancaishaun/fyp_test/master/statCollect.py
