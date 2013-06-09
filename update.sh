c88611e50a2e5a258f158942eb443f6da9ae945b
xuancaishaun <xuancaishaun@gmail.com>
2013-06-09 10:30:52 +0000

    changed url links to v0

--- update.sh
+++ update.sh
@@ -7,51 +7,51 @@ fi
 
 if [ "$1" == "a13" ]
 then
-	wget -O lib/external_gateway.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_a13/external_gateway.py
-	wget -O lib/scheduler.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_a13/scheduler.py
-	wget -O lib/app_worker.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_a13/app_worker.py
-	wget -O lib/gateway.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_a13/gateway.py
-	wget -O lib/appsocket.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_a13/appsocket.py
+	wget -O lib/external_gateway.py https://raw.github.com/xuancaishaun/fyp_test/v0/nep2p2_a13/external_gateway.py
+	wget -O lib/scheduler.py https://raw.github.com/xuancaishaun/fyp_test/v0/nep2p2_a13/scheduler.py
+	wget -O lib/app_worker.py https://raw.github.com/xuancaishaun/fyp_test/v0/nep2p2_a13/app_worker.py
+	wget -O lib/gateway.py https://raw.github.com/xuancaishaun/fyp_test/v0/nep2p2_a13/gateway.py
+	wget -O lib/appsocket.py https://raw.github.com/xuancaishaun/fyp_test/v0/nep2p2_a13/appsocket.py
 
-	wget -O lib/util/logger.py https://raw.github.com/xuancaishaun/fyp_test/master/logger.py
+	wget -O lib/util/logger.py https://raw.github.com/xuancaishaun/fyp_test/v0/logger.py
 
-	wget -O statCollect.py https://raw.github.com/xuancaishaun/fyp_test/master/statCollect.py
-	wget -O run.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_a13/run.py
-	wget -O cli.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_a13/cli.py
-	wget -O ddM16m8r92TO.txt https://raw.github.com/xuancaishaun/fyp_test/master/ddM16m8r92TO.txt
+	wget -O statCollect.py https://raw.github.com/xuancaishaun/fyp_test/v0/statCollect.py
+	wget -O run.py https://raw.github.com/xuancaishaun/fyp_test/v0/nep2p2_a13/run.py
+	wget -O cli.py https://raw.github.com/xuancaishaun/fyp_test/v0/nep2p2_a13/cli.py
+	wget -O ddM16m8r92TO.txt https://raw.github.com/xuancaishaun/fyp_test/v0/ddM16m8r92TO.txt
 
 elif [ "$1" == "a17" ] || [ "$1" == 'a17_rudp' ]
 then
-	wget -O lib/external_gateway.py https://raw.github.com/xuancaishaun/fyp_test/master/external_gateway.py
-	wget -O lib/internal_gateway.py https://raw.github.com/xuancaishaun/fyp_test/master/internal_gateway.py
-	wget -O lib/scheduler.py https://raw.github.com/xuancaishaun/fyp_test/master/scheduler.py
-	wget -O lib/app_worker.py https://raw.github.com/xuancaishaun/fyp_test/master/app_worker.py
-	wget -O lib/gateway.py https://raw.github.com/xuancaishaun/fyp_test/master/gateway.py
-	wget -O lib/appsocket.py https://raw.github.com/xuancaishaun/fyp_test/master/appsocket.py
-	wget -O lib/connection.py https://raw.github.com/xuancaishaun/fyp_test/master/connection.py
-	wget -O lib/batch_acknowledger.py https://raw.github.com/xuancaishaun/fyp_test/master/batch_acknowledger.py
+	wget -O lib/external_gateway.py https://raw.github.com/xuancaishaun/fyp_test/v0/external_gateway.py
+	wget -O lib/internal_gateway.py https://raw.github.com/xuancaishaun/fyp_test/v0/internal_gateway.py
+	wget -O lib/scheduler.py https://raw.github.com/xuancaishaun/fyp_test/v0/scheduler.py
+	wget -O lib/app_worker.py https://raw.github.com/xuancaishaun/fyp_test/v0/app_worker.py
+	wget -O lib/gateway.py https://raw.github.com/xuancaishaun/fyp_test/v0/gateway.py
+	wget -O lib/appsocket.py https://raw.github.com/xuancaishaun/fyp_test/v0/appsocket.py
+	wget -O lib/connection.py https://raw.github.com/xuancaishaun/fyp_test/v0/connection.py
+	wget -O lib/batch_acknowledger.py https://raw.github.com/xuancaishaun/fyp_test/v0/batch_acknowledger.py
 	
 
-	wget -O lib/util/logger.py https://raw.github.com/xuancaishaun/fyp_test/master/logger.py
-	wget -O lib/util/ip.py https://raw.github.com/xuancaishaun/fyp_test/master/ip.py
-	wget -O lib/util/bmessage.py https://raw.github.com/xuancaishaun/fyp_test/master/bmessage.py
+	wget -O lib/util/logger.py https://raw.github.com/xuancaishaun/fyp_test/v0/logger.py
+	wget -O lib/util/ip.py https://raw.github.com/xuancaishaun/fyp_test/v0/ip.py
+	wget -O lib/util/bmessage.py https://raw.github.com/xuancaishaun/fyp_test/v0/bmessage.py
 
-	wget -O statCollect.py https://raw.github.com/xuancaishaun/fyp_test/master/statCollect.py
-	wget -O run.py https://raw.github.com/xuancaishaun/fyp_test/master/run.py
-	wget -O cli.py https://raw.github.com/xuancaishaun/fyp_test/master/cli.py
-	wget -O ddM16m8r92TO.txt https://raw.github.com/xuancaishaun/fyp_test/master/ddM16m8r92TO.txt
+	wget -O statCollect.py https://raw.github.com/xuancaishaun/fyp_test/v0/statCollect.py
+	wget -O run.py https://raw.github.com/xuancaishaun/fyp_test/v0/run.py
+	wget -O cli.py https://raw.github.com/xuancaishaun/fyp_test/v0/cli.py
+	wget -O ddM16m8r92TO.txt https://raw.github.com/xuancaishaun/fyp_test/v0/ddM16m8r92TO.txt
 
 	if [ "$1" == "a17_rudp" ]
 	then
-		wget -O lib/appsocket.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/appsocket.py
-		wget -O lib/gateway.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/gateway.py
-		wget -O lib/rudp.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/rudp.py
-		wget -O run.py https://raw.github.com/xuancaishaun/fyp_test/master/nep2p2_rudp/run.py
+		wget -O lib/appsocket.py https://raw.github.com/xuancaishaun/fyp_test/v0/nep2p2_rudp/appsocket.py
+		wget -O lib/gateway.py https://raw.github.com/xuancaishaun/fyp_test/v0/nep2p2_rudp/gateway.py
+		wget -O lib/rudp.py https://raw.github.com/xuancaishaun/fyp_test/v0/nep2p2_rudp/rudp.py
+		wget -O run.py https://raw.github.com/xuancaishaun/fyp_test/v0/nep2p2_rudp/run.py
 	fi
 elif [ "$1" == "a17_rudp_to_a17" ]
 then
-	wget -O lib/gateway.py https://raw.github.com/xuancaishaun/fyp_test/master/gateway.py
-	wget -O lib/appsocket.py https://raw.github.com/xuancaishaun/fyp_test/master/appsocket.py
+	wget -O lib/gateway.py https://raw.github.com/xuancaishaun/fyp_test/v0/gateway.py
+	wget -O lib/appsocket.py https://raw.github.com/xuancaishaun/fyp_test/v0/appsocket.py
 
 elif [ "$1" == "a13_to_a17" ]
 then
