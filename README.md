f8c2eb473688d3bc492c6786d2f46135a9c5019e
xuancaishaun <xuancaishaun@gmail.com>
2013-06-09 10:16:51 +0000

    cleaned up codes

--- README.md
+++
@@ -1,3 +0,0 @@
-Nodes that do not work:
-
-s2 (cannot install openssh-server) ,s4 (failed to be connected),s6 (cannot start sshd service),s7 (cannot download from github.com)
--- scripts/wgetnep2p3.sh
+++
@@ -1,7 +0,0 @@
-mkdir danny-nep2p2-0.2
-cd danny-nep2p2-0.2
-wget http://lab.nep2p.com/danny.tar.gz
-tar -zxvf danny.tar.gz
-cd danny
-sudo chmod +x softwareinit3.sh
-sudo ./softwareinit3.sh
